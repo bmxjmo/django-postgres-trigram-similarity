1070d6213cc0d046309c5b8bbb92a8c527aa612e
uabj8000 <uabj8000@gmail.com>
2021-06-29 07:50:46 -0400

    fix blog model, verbose name categories

--- src/blog/models.py
+++ src/blog/models.py
@@ -12,6 +12,9 @@ def user_directory_path(instance, filename):
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name_plural = "categories"
+        
     def __str__(self):
         return self.name
 
